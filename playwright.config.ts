8002e070aa0ce10934ccbf9de5c1321fd33bb526
rainerhahnekamp <I1zL5Y9BLr>
2022-06-24 16:09:12 +0200

    add gitlab ci config

--- playwright.config.ts
+++ playwright.config.ts
@@ -31,7 +31,7 @@ const config: PlaywrightTestConfig = {
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: "html",
+  reporter: [["html", { outputFolder: "my-reports", open: "never" }]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     baseURL: "https://genuine-narwhal-f0f8ad.netlify.app/",
