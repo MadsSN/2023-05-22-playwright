e21b59e5cfcfbbae3ca091313339e86f3e7bde49
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2023-01-19 19:48:03 +0100

    update

--- push-branches.sh
+++ push-branches.sh
@@ -2,7 +2,7 @@ set -e
 
 git checkout main
 
-branches=(main solution-1-e2e-basics solution-2-e2e-advanced solution-3-unit-tests-async-mock solution-4-rxjs-ngrx solution-5-component-integration-tests solution-6-component-integration-tests-fallback solution-7-visual-regression)
+branches=(main 1-start 2-basics 3-advanced 4-beyond-e2e)
 
 for branch in ${branches[*]}; do
   git checkout $branch
